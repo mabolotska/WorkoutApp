e96abf71b7f28ad756bdfd364ce98b29818882f3
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-24 15:52:23 +0200

    custom navBar

---
+++ WorkoutApp/Application/Controllers/Base/BaseView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseView.swift
+//  WorkoutApp
+//
+//  Created by Maryna Bolotska on 24/07/23.
+//
+
+import Foundation
---
+++ WorkoutApp/Application/Controllers/Overview/Views/OverviewNavBar.swift
@@ -0,0 +1,8 @@
+//
+//  OverviewNavBar.swift
+//  WorkoutApp
+//
+//  Created by Maryna Bolotska on 24/07/23.
+//
+
+import Foundation
